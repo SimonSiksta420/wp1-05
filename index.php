32c24dcab38d006c3bb9bbbfab08d93e3e1fc9d8
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-03-08 11:11:48 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$homelessmoney; = 25
+$vodkaprice; = 19
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?= $homelessmoney; + 17 + 40 + 100 - 50 ?>
+<br> Peníze bezdomovce před návštěvou večerky <?= $homelessmoney; ?> <br>
+<br> Cena vodky <?= $vodkaprice; ?> </br> 
+
+<?= if ($homelessmoney; >= 119) {
+    $homelessmoney; - 119
+    $answer; = Děkujeme za váš nákup
+}
+else {
+    $answer; = Nemáte peníze na vodku naschledanou
+}
+?>
+
+<br> Peníze po návštěvě večerky <?= $homelessmoney ?>  </br>
+
+
+    
+</body>
+</html>
